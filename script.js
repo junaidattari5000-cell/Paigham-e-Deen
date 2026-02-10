9afd05deb0e822495c3eff43eb37ccf539a5635c
junaidattari5000-cell <junaidattari5000@gmail.com>
2026-02-10 10:39:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -272,3 +272,28 @@ extraBtns.forEach(btn => {
     btn.style.color = "#fff";
   });
 });
+const CACHE_NAME = 'paigham-deen-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './image/background.png',
+  './image/image1.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
